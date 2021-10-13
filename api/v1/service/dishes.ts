5bf7eeb62901013629d17ba1737625c407de49ec
Rabeafm <rabeaf@moveo.co.il>
2021-10-13 15:52:07 +0300

    Fixed add & delete for dishes and resturants

--- api/v1/service/dishes.ts
+++ api/v1/service/dishes.ts
@@ -1,9 +1,25 @@
+import { Request } from 'express';
 import BaseCRUD from './basecrud';
 import Dish from '../../../models/Dish';
+import Rest from '../../../models/Rest';
 
 export default new (class DishCRUD extends BaseCRUD {
-  
-  // change functions
-  //   add: async ({ body }: Request) => await Dish.create(body), // Add dish
-  //   delete: async ({ params: { id } }: Request) => await Dish.findByIdAndDelete(id), // Delete dish by id
+  // Override functions
+
+  public add = async ({ body }: Request) => {
+    let rest = await Rest.findById(body.resturant);
+    let dish = await Dish.create(body);
+    rest.dishes.push(dish);
+    rest.save();
+    return rest;
+  }; // Add Dish & Update Resturant
+
+  public delete = async ({ params: { id }, body }: Request) => {
+    let rest = await Rest.findById(body.resturant);
+    let dish = await Dish.findById(id);
+    rest.dishes.pull(dish);
+    rest.save();
+    dish.delete();
+    return dish;
+  }; // Delete Dish & Update Resturant
 })(Dish);
--- api/v1/service/rests.ts
+++ api/v1/service/rests.ts
@@ -1,22 +1,24 @@
+import { Request } from 'express';
 import BaseCRUD from './basecrud';
-import Rest from '../../../models/Dish';
+import Rest from '../../../models/Rest';
+import Chef from '../../../models/Chef';
 
 export default new (class DishCRUD extends BaseCRUD {
-  
   // change functions
-  //   add: async ({ body }: Request) => {
-  //     let chef = await Chef.findById(body.chef);
-  //     let rest = await Rest.create(body);
-  //     chef.rests.push(rest);
-  //     chef.update();
-  //     return rest;
-  //   }, // Add resturant
-  //   delete: async ({ params: { id }, body }: Request) => {
-  //     let chef = await Chef.findById(body.chef);
-  //     let rest = await Rest.findById(id);
-  //     chef.rests = chef.rests.filter((id: any) => id !== rest._id);
-  //     chef.update();
-  //     rest.delete();
-  //     return rest;
-  //   }, // Delete resturant by id
+  public add = async ({ body }: Request) => {
+    let chef = await Chef.findById(body.chef);
+    let rest = await Rest.create(body);
+    chef.rests.push(rest);
+    chef.save();
+    return rest;
+  }; // Add resturant & Update chef
+
+  public delete = async ({ params: { id }, body }: Request) => {
+    let chef = await Chef.findById(body.chef);
+    let rest = await Rest.findById(id);
+    chef.rests.pull(rest);
+    chef.save();
+    rest.delete();
+    return rest;
+  }; // Delete resturant & update chef
 })(Rest);
